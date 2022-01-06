523dd098a554ada1756927decb0847d823ec5f91
bharath2408 <bharatha24kumar@gmail.com>
2022-01-06 12:43:13 +0530

    java class

---
+++ SECTION_4/WorkBook2.1/Gryffindor.java
@@ -0,0 +1,13 @@
+public class Gryffindor{
+    public static void main(String[] args) {
+        
+        int gryffindor = 0;
+        gryffindor -= 50;
+        gryffindor -= 3;
+        gryffindor += 30;
+        gryffindor += 100;
+        gryffindor += 60;
+
+        System.out.println(gryffindor);
+    }
+}
