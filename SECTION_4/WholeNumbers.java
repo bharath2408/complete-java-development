9d71e2622e2c22ad05584e7f57cf9b21478493ff
bharath2408 <bharatha24kumar@gmail.com>
2022-01-08 18:27:29 +0530

    java class 3

--- SECTION_4/WholeNumbers.java
+++ SECTION_4/WholeNumbers.java
@@ -2,5 +2,8 @@ public class WholeNumbers{
     public static void main(String[] args) {
         int numberOfFans = 5000;
         System.out.println("There are "+ numberOfFans + " Fans in squid games");
+
+        long globelPopulation = 7000000000L;
+        System.out.println("There are " + globelPopulation + " population in world");
     }
 }
